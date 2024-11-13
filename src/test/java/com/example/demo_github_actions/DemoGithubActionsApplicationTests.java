11d9d1f4d7bfc306889b698baadf6b40784f0404
José Antonio Periñán <perinan2002@gmail.com>
2024-11-13 18:31:22 +0100

    First Version

---
+++ src/test/java/com/example/demo_github_actions/DemoGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_github_actions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGithubActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
